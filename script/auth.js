9be3acaa95b8755598aed477e4892d16147ac805
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-06-25 16:00:15 -0400

    pushing google fixes

--- script/auth.js
+++ script/auth.js
@@ -13,3 +13,32 @@ function handleCredentialResponse(response) {
 
     console.log(JSON.stringify(parseJwt(response.credential)));
 }
+
+function handleCredentialResponse(response) {
+    // decodeJwtResponse() is a custom function defined by you
+    // to decode the credential response.
+    const responsePayload = decodeJwtResponse(response.credential);
+
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+}
+
+function handleCredentialResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+}
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "176962672918-072st1hpbfv8pfhrtue6g5v6tjeb9mge.apps.googleusercontent.com",
+        callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }  // customization attributes
+    );
+    google.accounts.id.prompt(); // also display the One Tap dialog
+}
