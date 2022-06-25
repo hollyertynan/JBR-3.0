acaf1e11b5963f95f5581ae8776a90cc7559a023
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-06-25 17:21:54 -0400

    test decoding push

--- script/auth.js
+++ script/auth.js
@@ -2,7 +2,7 @@ function handleCredentialResponse(response) {
     // decodeJwtResponse() is a custom function defined by you
     // to decode the credential response.
     //const responsePayload = decodeJwtResponse(response.credential);
-    console.log(response.credential);
+    console.log(jwtDecode(response));
     //console.log("ID: " + responsePayload.sub);
     //console.log('Full Name: ' + responsePayload.name);
     //console.log('Given Name: ' + responsePayload.given_name);
