f023ef92e1ec80eedd34fd6b4cb8042b492daa98
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-06-25 16:11:32 -0400

    testing if sign in truly works

--- script/auth.js
+++ script/auth.js
@@ -1,19 +1,3 @@
-function parseJwt(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-};
-
-function handleCredentialResponse(response) {
-    //console.log(response)
-
-    console.log(JSON.stringify(parseJwt(response.credential)));
-}
-
 function handleCredentialResponse(response) {
     // decodeJwtResponse() is a custom function defined by you
     // to decode the credential response.
@@ -26,3 +10,5 @@ function handleCredentialResponse(response) {
     console.log("Image URL: " + responsePayload.picture);
     console.log("Email: " + responsePayload.email);
 }
+
+// data-callback="https://hollyertynan.github.io/JBR-3.0/"
