61bc713f7ef2bcbd59f86b848fd509c1c33e64ff
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-06-25 18:55:56 -0400

    please for the love of god I'm begging you just work please I need this to work so bad

--- script/auth.js
+++ script/auth.js
@@ -1,5 +1,15 @@
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+   var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+     return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+   }).join(''));
+ 
+   return JSON.parse(jsonPayload);
+};
+
 function handleCredentialResponse(response) {
-    const responsePayLoad = jwtDecode(response.credential);
+    const responsePayLoad = parseJwt(response.credential);
     console.log("ID: " + responsePayLoad.sub);
     console.log('Full Name: ' + responsePayLoad.name);
     console.log('Given Name: ' + responsePayLoad.given_name);
