474555d736b7dd09acb6eb4d0b58402e979c6778
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-06-25 17:47:01 -0400

    testing google information

--- script/auth.js
+++ script/auth.js
@@ -1,14 +1,10 @@
 function handleCredentialResponse(response) {
-    // decodeJwtResponse() is a custom function defined by you
-    // to decode the credential response.
-    //const responsePayload = decodeJwtResponse(response.credential);
-    console.log(jwtDecode(response));
-    //console.log("ID: " + responsePayload.sub);
-    //console.log('Full Name: ' + responsePayload.name);
-    //console.log('Given Name: ' + responsePayload.given_name);
-    //console.log('Family Name: ' + responsePayload.family_name);
-    //console.log("Image URL: " + responsePayload.picture);
-    //console.log("Email: " + responsePayload.email);
+    const responsePayLoad = jwtDecode(response.credential);
+    console.log("ID: " + responsePayLoad.sub);
+    console.log('Full Name: ' + responsePayLoad.name);
+    console.log('Given Name: ' + responsePayLoad.given_name);
+    console.log('Family Name: ' + responsePayLoad.family_name);
+    console.log("Image URL: " + responsePayLoad.picture);
+    console.log("Email: " + responsePayLoad.email); 
+    alert(responsePayLoad.email);
 }
-
-// data-callback="https://hollyertynan.github.io/JBR-3.0/"
