8393607c8d93e77120451a543e20863f97a4998a
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-06-25 13:47:58 -0400

    fix google auth

--- script/auth.js
+++ script/auth.js
@@ -1,10 +1,6 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
 
-    document.getElementById("yourName").innerHTML = "Welcome, "
-    document.getElementById("googleSigningForRemoval").innerHTML = "<h1>" + googleUser.getBasicProfile().getName() + "</h1>";
-    document.getElementById("googleProfilePicture").innerHTML = "<h1><img src='" + googleUser.getBasicProfile().getImageUrl() + "' alt='Profile Picture'></h1>";
-
     $("#profilePic").html(profile.getImageUrl());
     $("#name").html(profile.getName());
 }
