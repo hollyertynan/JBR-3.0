06a312560fe82ce54ff80625c3e8b5d759a94a78
damianjoakes <94327623+damianjoakes@users.noreply.github.com>
2022-06-26 09:21:43 -0400

    working(?) multisearch

--- script/script.js
+++ script/script.js
@@ -2,8 +2,27 @@ var Result = {
 
 }
 
-var searchList = []
+var globalSearch = []
 
+var wordExclusionFromSearch = ['the', 'to', 'and', 'or', 'where', 'what', 'a', 'an']
+
+function splitAndRefineSearchList(searchToSplit) {
+    var searchList = []
+    searchList = searchToSplit.split(" ")
+    let fuck
+    for (i in searchList) {
+        for (c in wordExclusionFromSearch) {
+            if (searchList[i] == wordExclusionFromSearch[c])  {
+                searchList.splice(wordExclusionFromSearch[c], 1)
+            } else {
+                continue;
+            }
+        }
+    }
+    globalSearch = searchList.map(element => {
+        return element.toLowerCase()
+    })
+}
 
 
 var tempText
@@ -14,6 +33,7 @@ function load() {
     // search term
     var resources = ["accounting.json", "IT.json", "product.json", "marketing.json"]
     var search = document.getElementById("searchBar").value;
+    splitAndRefineSearchList(search)
 
     // processes for each file from resources[]
     for (let i = 0; i < resources.length; i++) {
@@ -26,48 +46,54 @@ function load() {
             })
             .then(json => {
                 this.answer = json
-
                 //  length of json file loop
                 for (i = 0; i < this.answer.length; i++) {
                     this.answer[i].tags.forEach((tag) => {
-                        
                         //find tag in json object
-                        if (tag.toLowerCase() != search.toLowerCase()) {
-                            return;
-                        } else if (tag.toLowerCase() == search.toLowerCase() && tempTitle != tag.toLowerCase()) {
+                        if(!globalSearch.includes(tag.toLowerCase())) {
+                           return;
+                        } else if (globalSearch.includes(tag.toLowerCase()) && tempTitle != tag.toLowerCase()) {
+                            console.log(tag)
                             tempTitle = this.answer[i].title
                             tempText = this.answer[i].source
                             populateSearch();
                         } else {
                             return;
                         }
+                        /* if (tag.toLowerCase() != globalSearch[i].toLowerCase()) {
+                            return;
+                        } else if (tag.toLowerCase() == globalSearch[i].toLowerCase() && tempTitle != tag.toLowerCase()) {
+                            
+                        } else {
+                            return;
+                        } */
                     })
 
                 }
             })
     }
     console.log(Result)
-    
-    
+
+
 }
 
 function populateSearch() {
     Result.title = tempTitle
     Result.source = tempText
-    if(Result.title == undefined) {
+    if (Result.title == undefined) {
         return;
     }
     document.getElementById("searchResults").innerHTML += "<button class=\"list-group-item py-3\" onclick=\"window.location='" + Result.source + "';\">" + Result.title + "</button>"
     Result = {}
     $("#searchResults").fadeIn(250)
     $("button").on({
-        mouseenter: function(){
+        mouseenter: function () {
             $(this).css("background-color", "#f5f5f5");
         },
 
-        mouseleave: function(){
+        mouseleave: function () {
             $(this).css("background-color", "#FFFFFF");
         }
-        
+
     })
 }
