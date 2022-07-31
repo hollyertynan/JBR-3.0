2bb880c195aff60b5122e01c9e34cf804599c2e6
damianjoakes <94327623+damianjoakes@users.noreply.github.com>
2022-07-31 14:41:44 -0400

    Got that mf authentication changing, got that mf data validation, got that mf Register Ticket and Store number working, we on cracka

--- script/form-submission.js
+++ script/form-submission.js
@@ -1,6 +1,23 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function SubForm() {
+    let ticketNumber = document.getElementById("ticketCaller").value
+    let storeNumber = document.getElementById("storeNumber").value
+
+    if (authLevel >= 2 && ticketNumber != "" && storeNumber != "") {
+        document.forms['submitMyForm'].elements['Ticket/Caller'].value = ticketNumber;
+        document.forms['submitMyForm'].elements['Store Number'].value = storeNumber;
+    }  else if (authLevel >= 2 && ticketNumber == "" && storeNumber != "") {
+        showPrompts("ticketNumberAndStoreNotFilledIn")
+        document.getElementById("submitModal").disabled = false
+        return
+    } else if (authLevel >= 2 && ticketNumber != "" && storeNumber == "") {
+        showPrompts("ticketNumberAndStoreNotFilledIn")
+        document.getElementById("submitModal").disabled = false
+        return
+    }
+
+
     document.forms['submitMyForm'].elements['Start Time'].value = saveStartTime;
 
     var saveEndTime = new Date().toLocaleString();
@@ -10,20 +27,24 @@ function SubForm() {
     document.forms['submitMyForm'].elements['Department'].value = dept_name;
     document.forms['submitMyForm'].elements['Task Name'].value = task_name;
     document.forms['submitMyForm'].elements['Comments'].value = comments.value;
-
+    document.forms['submitMyForm'].elements['LocationFrom'].value = authLevel;
+    document.forms['submitMyForm'].elements['Register Number'].value = registerNumber;
+    
     document.getElementById("submittext").innerHTML = "";
     $("#submittext").addClass("spinner-border text-light");
     $("#submitcheck").removeClass("bi bi-check-circle");
     document.getElementById("submissionButton").disabled = true;
 
     $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbyWoBqrGjDVT2rSvEEg0BpPlvTZ_1PGUGbVTe-8B03cv4B2Ka1KFFzA89--G8daGSwDtw/exec",
+        url: "https://script.google.com/macros/s/AKfycbwW4J9Znq9yLZc9rU8OD8V4EzJ6ueRajiHWH5iLGynqY7awlG9VRF08brM5r_jzbRU/exec",
         type: "post",
         data:$("#submitMyForm").serializeArray(),
+        
         
         success: async function() {
             await delay(1000);
             location.reload();
+            //console.log(JSON.stringify($("#submitMyForm").serializeArray()));
             
         },
         error: async function() {
